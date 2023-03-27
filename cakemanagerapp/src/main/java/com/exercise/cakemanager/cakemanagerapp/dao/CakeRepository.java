493b1ccd04fc839ffc448d3c768e77379311a63c
sudha91 <kadambari.sudha@gmail.com>
2023-03-27 18:53:29 +0100

    Delete cakemanagerapp directory

--- cakemanagerapp/src/main/java/com/exercise/cakemanager/cakemanagerapp/dao/CakeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exercise.cakemanager.cakemanagerapp.dao;
-
-import com.exercise.cakemanager.cakemanagerapp.entity.Cake;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CakeRepository extends JpaRepository<Cake, Integer> {
-}
--- cakemanagerapp/src/main/resources/schema-mysql.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `cake`;
-
-CREATE TABLE `cake` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `title` varchar(300) NOT NULL,
-  `desc` varchar(300) NOT NULL,
-  `image` varchar(300) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
